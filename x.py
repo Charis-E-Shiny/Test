f6e3fecae4174f0ac0705c2f6ea22bff49d72576
Charis E Shiny <echaris.shiny@gmail.com>
2026-02-19 12:23:58 +0530

    added var as Caps in x.py

--- x.py
+++ x.py
@@ -1,6 +1,7 @@
-var_a = 2
-var_b = 3
-var_c = var_a+var_b
-print(var_c)
-var_d = var_a*var_b
-print(var_d)
+VAR_A = 2
+VAR_B = 3
+VAR_C = VAR_A + VAR_B
+print(VAR_C)
+
+VAR_D = VAR_A * VAR_B
+print(VAR_D)
